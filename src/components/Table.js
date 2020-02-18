6b6d472d1ecb6ef33b2ae0ee7d902b96f2cac858
Nicholas Duffy <duffn@outlook.com>
2020-02-17 20:47:58 -0700

    Make table component less than 100% width

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,10 +1,10 @@
-import React from "react";
-import MaterialTable from "material-table";
+import React from "react"
+import MaterialTable from "material-table"
 
 export default function Table({ title, data, columns }) {
   return (
     <MaterialTable
-      style={{ width: "100%" }}
+      style={{ width: "86.5%" }}
       title={title}
       columns={columns}
       data={data}
@@ -13,8 +13,8 @@ export default function Table({ title, data, columns }) {
         exportButton: true,
         pageSize: 25,
         pageSizeOptions: [25, 50, 100],
-        padding: "dense"
+        padding: "dense",
       }}
     />
-  );
+  )
 }
