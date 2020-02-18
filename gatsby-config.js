c13151df3d9e6b6863b088274432db288a8ff23f
Nicholas Duffy <duffn@outlook.com>
2020-02-18 16:29:18 -0700

    Add manifest plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -15,6 +15,18 @@ module.exports = {
         trackingId: process.env.GOOGLE_ANALYTICS_TRACKING_ID,
       },
     },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `The Outer Rim`,
+        short_name: `The Outer Rim`,
+        start_url: `/`,
+        background_color: `#ffffff`,
+        theme_color: `#033E8C`,
+        display: `minimal-ui`,
+        icon: `src/images/tor.png`,
+      },
+    },
   ],
   siteMetadata: {
     title: `The Outer Rim`,
