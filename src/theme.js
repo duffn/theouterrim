b0b136d8141acaed034e7d88d6a8c5af18ac51f3
Nicholas Duffy <duffn@outlook.com>
2020-02-18 14:39:57 -0700

    Update theme colors

--- src/theme.js
+++ src/theme.js
@@ -1,22 +1,22 @@
-import { red } from '@material-ui/core/colors';
-import { createMuiTheme } from '@material-ui/core/styles';
+import { red } from "@material-ui/core/colors"
+import { createMuiTheme } from "@material-ui/core/styles"
 
 // A custom theme for this app
 const theme = createMuiTheme({
   palette: {
     primary: {
-      main: '#556cd6',
+      main: "#033E8C",
     },
     secondary: {
-      main: '#19857b',
+      main: "#0455BF",
     },
     error: {
       main: red.A400,
     },
     background: {
-      default: '#fff',
+      default: "#FFFFFF",
     },
   },
-});
+})
 
-export default theme;
+export default theme
