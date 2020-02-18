a056454ff4071bf9c353648a6d3a0518c32c9035
Nicholas Duffy <duffn@outlook.com>
2020-02-18 11:20:46 -0700

    Remove unused import from IndividualCard

--- src/components/shared/IndividualCard.js
+++ src/components/shared/IndividualCard.js
@@ -4,7 +4,6 @@ import Card from "@material-ui/core/Card"
 import CardContent from "@material-ui/core/CardContent"
 import Typography from "@material-ui/core/Typography"
 
-import Dashboard from "../../components/Dashboard"
 import SEO from "../../components/SEO"
 
 const useStyles = makeStyles({
