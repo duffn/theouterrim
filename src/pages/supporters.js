22ce2651dea99d8b3ad5c0bf13633d27c59b1594
Nicholas Duffy <duffn@outlook.com>
2020-02-18 18:18:46 -0700

    Update supports page styling

--- src/pages/supporters.js
+++ src/pages/supporters.js
@@ -1,12 +1,22 @@
 import React from "react"
 import Container from "@material-ui/core/Container"
 import Typography from "@material-ui/core/Typography"
+import { makeStyles } from "@material-ui/core/styles"
 
 import Dashboard from "../components/shared/Dashboard"
 import Link from "../components/shared/Link"
 import SEO from "../components/shared/SEO"
 
+const useStyles = makeStyles(theme => ({
+  title: {
+    fontFamily: "Saira Semi Condensed",
+    marginTop: "2rem",
+  },
+}))
+
 const Supporters = () => {
+  const classes = useStyles()
+
   return (
     <Dashboard>
       <SEO title="Supporters" />
@@ -19,19 +29,37 @@ const Supporters = () => {
           </Link>
           .
         </Typography>
-        <Typography component="h1" variant="h2" color="primary" gutterBottom>
+        <Typography
+          className={classes.title}
+          component="h1"
+          variant="h2"
+          color="primary"
+          gutterBottom
+        >
           I Am The Senate
         </Typography>
         <Typography component="p" gutterBottom>
           Nobody here yet.
         </Typography>
-        <Typography component="h1" variant="h3" color="primary" gutterBottom>
+        <Typography
+          className={classes.title}
+          component="h1"
+          variant="h3"
+          color="primary"
+          gutterBottom
+        >
           Jedi
         </Typography>
         <Typography component="p" gutterBottom>
           Nobody here yet.
         </Typography>
-        <Typography component="h1" variant="h4" color="primary" gutterBottom>
+        <Typography
+          className={classes.title}
+          component="h1"
+          variant="h4"
+          color="primary"
+          gutterBottom
+        >
           Padawan
         </Typography>
         <Typography component="p" gutterBottom>
