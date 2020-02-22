5851ed8862c1ce2fe4150442c496b8d9ed636d89
Andre Lemay <lemay.andre@gmail.com>
2020-02-22 17:36:53 -0500

    Move previous styles into theme override Add "All" pagination option

--- src/components/shared/Table.js
+++ src/components/shared/Table.js
@@ -1,45 +1,53 @@
 import React from "react"
-import MaterialTable from "material-table"
 import MUIDatatable from "mui-datatables"
 import Typography from "@material-ui/core/Typography"
+import { createMuiTheme, MuiThemeProvider } from "@material-ui/core/styles"
 
 export default function Table({ title, data, columns, marginTop, grouping }) {
-  return (
-    <MUIDatatable
-      style={{
-        maxWidth: "100%",
-        width: "100%",
-        marginBottom: "1rem",
-        marginTop: marginTop != undefined ? "1rem" : null,
-      }}
-      //stickyHeader
-      title={
-        <Typography
-          variant="h6"
-          style={{
-            whiteSpace: "nowrap",
-            overflow: "hidden",
-            textOverflow: "ellipsis",
-            fontFamily: "Saira Semi Condensed",
-          }}
-        >
-          {title}
-        </Typography>
-      }
-      columns={columns}
-      data={data}
-      options={{
-        sort: true,
-        download: true,
-        downloadOptions: {
-          filename: `${title.replace(" ", "-")}_download`
+  let getMuiTheme = () =>
+    createMuiTheme({
+      overrides: {
+        MUIDataTable: {
+          responsiveStacked: {
+            marginBottom: "1rem",
+            marginTop: marginTop != undefined ? "1rem" : null,
+            height: "100vh",
+          },
         },
-        rowsPerPage: 25,
-        rowsPerPageOptions: [25, 50, 100, { value: -1, label: "All"}],
-        setTableProps: () => ({
-          size: "small" //for "dense" look
-        })
-      }}
-    />
+      },
+    })
+
+  return (
+    <MuiThemeProvider theme={getMuiTheme()}>
+      <MUIDatatable
+        title={
+          <Typography
+            variant="h6"
+            style={{
+              whiteSpace: "nowrap",
+              overflow: "hidden",
+              textOverflow: "ellipsis",
+              fontFamily: "Saira Semi Condensed",
+            }}
+          >
+            {title}
+          </Typography>
+        }
+        columns={columns}
+        data={data}
+        options={{
+          sort: true,
+          download: true,
+          downloadOptions: {
+            filename: `${title.replace(" ", "-")}_download`,
+          },
+          rowsPerPage: 25,
+          rowsPerPageOptions: [25, 50, 100, { value: data.length, label: "All" }],
+          setTableProps: () => ({
+            size: "small", //for "dense" look
+          }),
+        }}
+      />
+    </MuiThemeProvider>
   )
 }
