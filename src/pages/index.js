3e2603537a9cefeec15e9111d8ee3f402af372f2
Nicholas Duffy <duffn@outlook.com>
2020-03-02 20:26:45 -0700

    Try different input props

--- src/pages/index.js
+++ src/pages/index.js
@@ -52,6 +52,8 @@ export default function Index() {
                     <SearchIcon />
                   </InputAdornment>
                 ),
+              }}
+              inputProps={{
                 "aria-label": "main search",
               }}
               variant="outlined"
