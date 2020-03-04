5f47ffa8e77241bd2655e5f98a342c680000ebae
Nicholas Duffy <duffn@outlook.com>
2020-03-03 19:50:53 -0700

    Make weapons attachment encum. and rarity strings

--- src/components/WeaponAttachmentsColumnProvider.js
+++ src/components/WeaponAttachmentsColumnProvider.js
@@ -4,52 +4,58 @@ import {
   makeColumns,
   RESTRICTED_COL_INDEX,
   GENERATED_ID_COL_INDEX,
-  indexRender
+  indexRender,
 } from "./shared/ColumnHelper"
 import ProvideBookData from "./shared/BookDataProvider"
 
-export default function WeaponAttachmentsColumnProvider({children, currentBook}){
+export default function WeaponAttachmentsColumnProvider({
+  children,
+  currentBook,
+}) {
   let bookData = ProvideBookData()
-  let columns = makeColumns([
-    {
-      label: "Name",
-      name: "name",
-      options: {
-        customBodyRender: (value, tableMeta) => (
-          <Link
-            to={`/weapon-attachments/${tableMeta.rowData[GENERATED_ID_COL_INDEX]}/`}
-          >
-            {value}
-          </Link>
-        ),
-        sortDirection: "asc",
-        filter: false,
+  let columns = makeColumns(
+    [
+      {
+        label: "Name",
+        name: "name",
+        options: {
+          customBodyRender: (value, tableMeta) => (
+            <Link
+              to={`/weapon-attachments/${tableMeta.rowData[GENERATED_ID_COL_INDEX]}/`}
+            >
+              {value}
+            </Link>
+          ),
+          sortDirection: "asc",
+          filter: false,
+        },
       },
-    },
-    { label: "Category", name: "category" },
-    {
-      label: "Price",
-      name: "price",
-      options: {
-        customBodyRender: (value, tableMeta) =>
-          `${
-            tableMeta.rowData[RESTRICTED_COL_INDEX] ? "(R) " : ""
-          }${value.toLocaleString()}`,
+      { label: "Category", name: "category" },
+      {
+        label: "Price",
+        name: "price",
+        options: {
+          customBodyRender: (value, tableMeta) =>
+            `${
+              tableMeta.rowData[RESTRICTED_COL_INDEX] ? "(R) " : ""
+            }${value.toLocaleString()}`,
+        },
       },
-    },
-    { label: "Encum.", name: "encumbrance" },
-    { label: "HP", name: "hp" },
-    { label: "Rarity", name: "rarity" },
-    {
-      label: "Index",
-      name: "index",
-      options: {
-        filter: false,
-        customBodyRender: (value, tableMeta) =>
-          indexRender(value, tableMeta, bookData, currentBook),
+      { label: "Encum.", name: "encumbrance", options: { sort: false } },
+      { label: "HP", name: "hp" },
+      { label: "Rarity", name: "rarity", options: { sort: false } },
+      {
+        label: "Index",
+        name: "index",
+        options: {
+          filter: false,
+          customBodyRender: (value, tableMeta) =>
+            indexRender(value, tableMeta, bookData, currentBook),
+        },
       },
-    },
-  ], true)
+    ],
+    true
+  )
 
   return React.cloneElement(React.Children.only(children), { columns })
 }
