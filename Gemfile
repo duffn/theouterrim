4593af31dcdfa5e94e0ea95dca6babdb700ae6bd
duffn <3457341+duffn@users.noreply.github.com>
2024-06-27 18:25:39 -0600

    Add dice icons

--- Gemfile
+++ Gemfile
@@ -3,6 +3,7 @@
 source 'https://rubygems.org'
 
 gem 'jekyll', '~> 4.3', '>= 4.3.3'
+gem 'kramdown', '~> 2.4'
 gem 'webrick', '~> 1.8', '>= 1.8.1'
 
 group :jekyll_plugins do
---
+++ _plugins/add_dice_icons_filter.rb
@@ -0,0 +1,30 @@
+require 'kramdown'
+
+module Jekyll
+  module AddDiceIconsFilter
+    def add_dice_icons(input)
+      return '' if input.nil?
+
+      replacements = {
+        '[BOOST]' => '<span class="eote" title="BOOST">B</span>',
+        '[DARK]' => '<span class="eote" title="DARK">z</span>',
+        '[LIGHT]' => '<span class="eote" title="LIGHT">Z</span>',
+        '[FORCE]' => '<span class="eote" title="FORCE">C</span>',
+        '[DESPAIR]' => '<span class="eote" title="DESPAIR">y</span>',
+        '[TRIUMPH]' => '<span class="eote" title="TRIUMPH">x</span>',
+        '[SUCCESS]' => '<span class="eote" title="SUCCESS">s</span>',
+        '[FAILURE]' => '<span class="eote" title="FAILURE">f</span>',
+        '[ADVANTAGE]' => '<span class="eote" title="ADVANTAGE">a</span>',
+        '[THREAT]' => '<span class="eote" title="THREAT">t</span>'
+      }
+
+      replacements.each do |key, value|
+        input = input.gsub(key, value)
+      end
+
+      Kramdown::Document.new(input).to_html
+    end
+  end
+end
+
+Liquid::Template.register_filter(Jekyll::AddDiceIconsFilter)
